562113342c5c8425d072c5cb60adcf66e6e210c4
NazarStf <hworker98@gmail.com>
2023-05-22 12:17:48 +0300

    new color to 3 bmiValue 's

--- BMI Calculator/Models/CalculatorBrain.swift
+++ BMI Calculator/Models/CalculatorBrain.swift
@@ -12,17 +12,18 @@ struct CalculatorBrain {
 	
 	var bmi: BMI?
 	
-	mutating func calculateBMI(height: Float , weight: Float) {
+	mutating func calculateBMI(height: Float, weight: Float) {
 		let bmiValue = weight / (height * height)
 		
 		if bmiValue < 18.5 {
-			bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor.cyan)
+			bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: UIColor(red: 22/255, green: 160/255, blue: 133/255, alpha: 1.0))
 		} else if bmiValue < 24.9 {
-			bmi = BMI(value: bmiValue, advice: "Fir as a fiddle!", color: UIColor.green)
+			bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: UIColor(red: 41/255, green: 128/255, blue: 185/255, alpha: 1.0))
 		} else {
-			bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor.red)
+			bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: UIColor(red: 192/255, green: 57/255, blue: 43/255, alpha: 1.0))
 		}
 	}
+
 	
 	func getBMIValue() -> String {
 		let bmiTo1DecimalPlace = String(format: "%.1f", bmi?.value ?? 0.0)
